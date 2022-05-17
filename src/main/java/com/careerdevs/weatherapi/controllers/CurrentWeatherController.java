a1492c85e24b347a0f3d9aa2e6a3494ffa2b3cad
PierreEdFils <epierrefils@gmail.com>
2022-05-17 19:45:07 -0400

    Create current controller

---
+++ src/main/java/com/careerdevs/weatherapi/controllers/CurrentWeatherController.java
@@ -0,0 +1,9 @@
+package com.careerdevs.weatherapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/current")
+public class CurrentWeatherController {
+}
