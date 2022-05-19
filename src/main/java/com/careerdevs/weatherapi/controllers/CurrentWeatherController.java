77a27b9e207657c54bc395e9fe039c3efee66b10
PierreEdFils <epierrefils@gmail.com>
2022-05-18 22:54:16 -0400

    Weather Api full

--- src/main/java/com/careerdevs/weatherapi/controllers/CurrentWeatherController.java
+++ src/main/java/com/careerdevs/weatherapi/controllers/CurrentWeatherController.java
@@ -1,9 +1,78 @@
 package com.careerdevs.weatherapi.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.careerdevs.weatherapi.models.CurrentWeather;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/current")
 public class CurrentWeatherController {
+
+    @Autowired
+    private Environment env;
+
+    private final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<?> getCurrentWeatherByCityPV (RestTemplate restTemplate, @PathVariable String cityName){
+
+        try {
+
+            String apikey = env.getProperty("OW_API_kEY");
+            String queryString = "?q=" + cityName + "&appid="+ apikey + "&units=imperial";
+            String openWeatherURL = BASE_URL + queryString;
+
+            CurrentWeather openWeatherResponse = restTemplate.getForObject(openWeatherURL,CurrentWeather.class);
+
+
+            assert openWeatherResponse != null;
+            System.out.println("City: " + openWeatherResponse.getName());
+            System.out.println("Temp: " + openWeatherResponse.getMain().getTemp()+ " F");
+            System.out.println("Desc: " + openWeatherResponse.getWeather()[0].getDescription());
+
+            return ResponseEntity.ok(openWeatherResponse);
+
+        } catch (HttpClientErrorException.NotFound e){
+            return ResponseEntity.status(404).body("City Not Found "+ cityName);
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
+
+    }
+//    @GetMapping("/city")
+//    public ResponseEntity<?> getCurrentWeatherByCityRP (RestTemplate restTemplate, @RequestParam String cityName){
+//
+//        try {
+//
+//            String apikey = env.getProperty("OW_API_kEY");
+//            String queryString = "?q=" + cityName + "&appid="+ apikey + "&units=imperial";
+//            String openWeatherURL = BASE_URL + queryString;
+//
+//            CurrentWeather openWeatherResponse = restTemplate.getForObject(openWeatherURL,CurrentWeather.class);
+//
+//            return ResponseEntity.ok(openWeatherResponse);
+//
+//        } catch (HttpClientErrorException.NotFound e){
+//            return ResponseEntity.status(404).body("City Not Found "+ cityName);
+//
+//        } catch (Exception e){
+//            System.out.println(e.getMessage());
+//            System.out.println(e.getClass());
+//
+//            return ResponseEntity.internalServerError().body(e.getMessage());
+//        }
+//
+//
+//    }
+
 }
---
+++ src/main/java/com/careerdevs/weatherapi/models/CurrentWeather.java
@@ -0,0 +1,141 @@
+package com.careerdevs.weatherapi.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class CurrentWeather  {
+
+
+
+    private  String name;
+
+    private int timezone;
+
+    private int visibility;
+
+    private Coords coord;
+
+    private Main main;
+
+    private Weather[] weather;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTimezone() {
+        return timezone;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public Coords getCoord() {
+        return coord;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public Weather[] getWeather() {
+        return weather;
+    }
+
+//        "weather": [
+//        {
+//        "id": 804,
+//        "main": "Clouds",
+//        "description": "overcast clouds",
+//        "icon": "04n"
+//        }
+
+    public static class Coords {
+        private float lon;
+        private float lat;
+
+        public float getLon() {
+            return lon;
+        }
+
+        public float getLat() {
+            return lat;
+        }
+    }
+
+    public static  class Main {
+
+        private float temp;
+        private float feels_like;
+        private float temp_min;
+        private float temp_max;
+        private int pressure;
+        private float humidity;
+
+        public float getTemp() {
+            return temp;
+        }
+
+        public float getFeels_like() {
+            return feels_like;
+        }
+
+        public float getTemp_min() {
+            return temp_min;
+        }
+
+        public float getTemp_max() {
+            return temp_max;
+        }
+
+        public int getPressure() {
+            return pressure;
+        }
+
+        public float getHumidity() {
+            return humidity;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "temp=" + temp +
+                    ", feels_like=" + feels_like +
+                    ", temp_min=" + temp_min +
+                    ", temp_max=" + temp_max +
+                    ", pressure=" + pressure +
+                    ", humidity=" + humidity +
+                    '}';
+        }
+    }
+
+    public static  class Weather {
+        //        {
+//        "id": 804,
+//        "main": "Clouds",
+//        "description": "overcast clouds",
+//        "icon": "04n"
+//        }
+        private int id;
+        private String main;
+        private String description;
+        private String icon;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getMain() {
+            return main;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+    }
+}
